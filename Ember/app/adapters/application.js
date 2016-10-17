b37e4a5fe9c598eb7f83a75107b16cb07828135e
Alex Kluew <dropbyalex@gmail.com>
2016-10-17 10:35:01 -0400

    Forgot to add the Authorizer mixin reference file.

--- Ember/app/adapters/application.js
+++ Ember/app/adapters/application.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 const { underscore, pluralize } = Ember.String;
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:devise',
   host: 'http://localhost:3000',
 
