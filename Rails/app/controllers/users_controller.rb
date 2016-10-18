87d4ccc23f4f2795911010620ef1e256057a1ab0
Alex Kluew <dropbyalex@gmail.com>
2016-10-18 14:45:39 -0400

    Added some test for users controller. Mainly to test travis.

--- Rails/app/controllers/users_controller.rb
+++ Rails/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    if params[:filter].present?
+    if params[:filter].present? && params[:filter][:user_type].present?
       user_type = params[:filter][:user_type]
       if user_type == "employee"
         users = User.where(employee: true)
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
     else
       users = User.all
     end
-    unless users.empty?
+
+    if users && !users.empty?
       render json: users, status: :ok
     else
       render json: { error: 'bad request' }, 	status: :bad_request
@@ -17,6 +18,10 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by_id(params[:id])
-    render json: user, status: :ok
+    if user
+      render json: user, status: :ok
+    else
+      render json: { error: 'this user does not exist' }, 	status: :not_found
+    end
   end
 end
--- Rails/db/seeds.rb
+++ Rails/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({email: 'test@test.com', password: 'password', first_name: 'Tester', last_name: 'Testing', admin: true, employee: true})
+User.create({email: 'batman@batman.com', password: 'password', first_name: 'Bruce', last_name: 'Wayne', admin: false, employee: true})
