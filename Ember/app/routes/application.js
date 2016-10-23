db1682cdedc40cabffbc382d24f71ce4a8ef5f02
Leann Labelle <leann.88.labelle@gmail.com>
2016-10-22 23:00:55 -0400

    Improve toggle of nav bar
    The nav bar now closes when using a mobile device after selecting the
    page to be viewed.

--- Ember/app/routes/application.js
+++ Ember/app/routes/application.js
@@ -19,3 +19,10 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
     return this.get('currentUser').load();
   }
 });
+
+//Used to toggle the nav bar closed when nav-item is clicked
+$(document).on('click','.collapse.in',function(e) {
+  if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown' ) ) {
+      $(this).collapse('hide');
+  }
+});
