8b76e96a2655f52b2a200c206d5a4ddcbee7c168
Alex Kluew <dropbyalex@gmail.com>
2016-10-26 14:51:15 -0400

    Fixing date serialization.

--- Ember/app/controllers/my-calendar/appointments/new.js
+++ Ember/app/controllers/my-calendar/appointments/new.js
@@ -8,12 +8,14 @@ export default Ember.Controller.extend({
       var self = this;
 
       function transitionToPost() {
+        Ember.$('#myModal').modal('hide');
         self.transitionToRoute('my-calendar');
       }
 
       function failure(reason) {
         // handle the error
-        debugger;
+        console.error('There was an error saving an appointment: ');
+        console.error(reason);
       }
 
       appointment.save().then(transitionToPost).catch(failure);
--- Ember/app/models/appointment.js
+++ Ember/app/models/appointment.js
@@ -6,7 +6,7 @@ export default Model.extend({
   color:      attr('string', { defaultValue: '#AB00FF' }),
   textColor:  attr('string', { defaultValue: '#FFFFFF' }),
   title:      attr('string', { defaultValue: 'New Appointment' }),
-  start:      attr('date', { defaultValue: new Date("2016-10-25T09:10") }),
-  end:        attr('date', { defaultValue: new Date("2016-10-26T09:10") }),
+  start:      attr('isodate'),
+  end:        attr('isodate'),
   notes:      attr('string')
 });
