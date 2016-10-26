7c1e10ed67832578030875497266a132f8583dfe
Leann Labelle <leann.88.labelle@gmail.com>
2016-10-25 22:30:53 -0400

    Add test for nav bar when not logged in

--- Ember/tests/integration/components/navigation-bar-test.js
+++ Ember/tests/integration/components/navigation-bar-test.js
@@ -1,11 +1,9 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import startApp from '../../helpers/start-app'; //Import for allowing ember visit function
-import Ember from 'ember';
 
 // Stub for session service
 const sessionStub = Ember.Service.extend({
-  isAuthenticated: false,
+  isAuthenticated: true,
 
   getIsAuthenticated() {
     return this.get('isAuthenticated');
@@ -27,7 +25,7 @@ const currentUserStub = Ember.Service.extend({
 moduleForComponent('navigation-bar', 'Integration | Component | navigation bar', {
   integration: true,
 
-  beforeEach: function() {
+  login : function() {
     this.register('service:session', sessionStub);
     this.register('service:current-user', currentUserStub);
     this.inject.service('session', { as: 'session' });
@@ -35,79 +33,26 @@ moduleForComponent('navigation-bar', 'Integration | Component | navigation bar',
   }
 });
 
-var App;
+test('it renders employee login bar', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+  assert.expect(2);
 
-module('Integration Test', {
-  setup: function(){
-    App = startApp();
-  },
-  teardown: function(){
-    Ember.run(App, 'destroy');
-  }
+  this.render(hbs`{{navigation-bar}}`);
+
+  assert.equal(this.$('.nav-item').length, 1, 'should be only 1 clickable link');
+  assert.equal(this.$('.nav-list-item').text(), 'Login', 'Navigation option is Login');
 });
 
-test('Should redirect to Login page.' , function (assert) {
-  visit('/');
-  click('a:contains("Login")');
-  andThen(function () {
-    assert.equal(currentURL(), '/login', 'should navigate to Login page.');
-  });
+test('it renders administrator\' s navigation bar', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.login();
+  debugger;
+  assert.expect(2);
+  this.render(hbs`{{navigation-bar}}`);
+
+  assert.equal(this.$('.nav-item').length, 6, 'should be only 6 clickable link');
+  assert.equal(this.$('.nav-list-item').text(), 'Login', 'start times should match');
 });
 
-test('Should redirect to root path after login' , function (assert) {
-  visit('/login');
-  fillIn('#inputEmail', 'test@test.com');
-  fillIn('#inputPassword', 'password');
-  click('.login-button');
-  andThen(function () {
-    assert.equal(currentURL(), '/login', 'should navigate to root path.');
-  });
-});
-
-test('Should redirect to Administrator\'\ s home.' , function (assert) {
-  visit('/');
-  click('a:contains("Home")');
-  andThen(function () {
-    assert.equal(currentURL(), '/admin-home', 'should navigate to administrator\'\ s homepage.');
-  });
-});
-
-test('Should redirect to Administrator\'\ s calendar.', function (assert) {
-  visit('/');
-  click('a:contains("My Calendar")');
-  andThen(function () {
-    assert.equal(currentURL(), '/my-calendar', 'should navigate to administrator\'\ s calendar.');
-  });
-});
-
-test('Should redirect to Administrator\'\ s manage account information.', function (assert) {
-  visit('/');
-  click('a:contains("My Account")');
-  andThen(function () {
-    assert.equal(currentURL(), '/my-account', 'should navigate to administrator\'\ s account information.');
-  });
-});
-
-test('Should redirect to Administrator\'\ s manage services page.', function (assert) {
-  visit('/');
-  click('a:contains("Services")');
-  andThen(function () {
-    assert.equal(currentURL(), '/services', 'should navigate to administrator\'\ s manage services page.');
-  });
-});
-
-test('Should redirect to Administrator\'\ s manage clients page.', function (assert) {
-  visit('/');
-  click('a:contains("Clients")');
-  andThen(function () {
-    assert.equal(currentURL(), '/clients', 'should navigate to administrator\'\ s manage clients page.');
-  });
-});
-
-test('Should redirect to Administrator\'\ s manage employees page.', function (assert) {
-  visit('/');
-  click('a:contains("Employees")');
-  andThen(function () {
-    assert.equal(currentURL(), '/employees', 'should navigate to administrator\'\ s manage employees page.');
-  });
-});
