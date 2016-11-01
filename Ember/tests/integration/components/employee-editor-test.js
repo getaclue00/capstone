6962787471b34fd8f5e3d6fcc5856c7106da90c3
Alex Kluew <dropbyalex@gmail.com>
2016-11-01 02:14:58 -0400

    Redid index page that lists all employees

--- Ember/tests/integration/components/employee-editor-test.js
+++ Ember/tests/integration/components/employee-editor-test.js
@@ -5,20 +5,16 @@ moduleForComponent('employee-editor', 'Integration | Component | employee editor
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders default view with no model', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  assert.expect(5);
 
   this.render(hbs`{{employee-editor}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#employee-editor}}
-      template block text
-    {{/employee-editor}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('input[type="text"]').length, 6, 'should be 6 text fields');
+  assert.equal(this.$('input[type="email"]').length, 1, 'should be 1 email field');
+  assert.equal(this.$('input[type="tel"]').length, 1, 'should be 1 telephone field');
+  assert.equal(this.$('input[type="number"]').length, 1, 'should be 1 numeric field');
+  assert.equal(this.$('input[type="datetime-local"]').length, 1, 'should be 1 datetime-local field');
 });
