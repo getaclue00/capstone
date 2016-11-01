6241a20b7ca493acbfbc48b440c875086d1ce628
Alex Kluew <dropbyalex@gmail.com>
2016-10-31 23:06:22 -0400

    Adding employee model

---
+++ Ember/app/models/employee.js
@@ -0,0 +1,15 @@
+import DS from 'ember-data';
+
+const { Model, attr } = DS;
+
+export default Model.extend({
+  lastName:     attr('string'),
+  firstName:    attr('string'),
+  email:        attr('string'),
+  phoneNumber:  attr('string'),
+  streetNumber: attr('string'),
+  city:         attr('string'),
+  province:     attr('string'),
+  postalCode:   attr('string'),
+  startDate:    attr('isodate')
+});
