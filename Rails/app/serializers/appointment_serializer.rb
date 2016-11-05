352f1e94e7be0fa19e0d6d19f3e76e298211b0d2
NadaRadwan <radwanmohsenn@gmail.com>
2016-11-05 11:28:57 -0400

    removed option from association is serializer

--- Rails/app/serializers/appointment_serializer.rb
+++ Rails/app/serializers/appointment_serializer.rb
@@ -2,6 +2,5 @@ class AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :color, :text_color, :title, :start, :end, :notes, :status
   belongs_to :car
   belongs_to :service
-  belongs_to :employee, optional: true #appointment may not have employee assigned at time of creation
-  #this is needed because belongs_to default behaviour doesnt allow this 
+  belongs_to :employee
 end
