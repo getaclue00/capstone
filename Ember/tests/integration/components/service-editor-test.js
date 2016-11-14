5e9910b381b52be4d7590846ddde7a90c3c7d4bb
Leann Labelle <leann.88.labelle@gmail.com>
2016-11-14 08:22:08 -0500

    More change requests implemented

--- Ember/tests/integration/components/service-editor-test.js
+++ Ember/tests/integration/components/service-editor-test.js
@@ -45,34 +45,26 @@ moduleForComponent('service-editor', 'Integration | Component | service editor',
   integration: true
 });
 
-test('it renders default view', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.set('model', service);
-  assert.expect(5);
-
-  this.render(hbs`{{service-editor model=model}}`);
-
-  assert.equal(this.$('input[type="name"]').length, 1, 'should be only 1 input name field - name of service');
-  assert.equal(this.$('input[type="number-minutes"]').length, 1, 'should be only 1 input number-minutes - Duration');
-  assert.equal(this.$('input[type="dollar-amount"]').length, 2, 'should be only 2 input dollar-amount for price of large and small cars');
-  assert.equal(this.$('select').length, 2, 'should be only 2 yes or no input for active and displayable attributes of service');
-  assert.equal(this.$('textarea').length, 1, 'should be only 1 text area for the description');
-});
-
 test('it renders a view with a model', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
   this.set('model', service);
 
-  assert.expect(7);
+  assert.expect(12);
 
   this.render(hbs`{{
     service-editor
     model=model
   }}`);
 
+  // Test in correct inputs bar are present
+  assert.equal(this.$('input[type="name"]').length, 1, 'should be only 1 input name field - name of service');
+  assert.equal(this.$('input[type="number-minutes"]').length, 1, 'should be only 1 input number-minutes - Duration');
+  assert.equal(this.$('input[type="dollar-amount"]').length, 2, 'should be only 2 input dollar-amount for price of large and small cars');
+  assert.equal(this.$('select').length, 2, 'should be only 2 yes or no input for active and displayable attributes of service');
+  assert.equal(this.$('textarea').length, 1, 'should be only 1 text area for the description');
+
+  // Test if proper values are placed into form
   assert.equal(this.$('input[type="name"]').val(), this.get('model.name'), 'names should match');
   assert.equal(this.$('input[type="number-minutes"]').val(), this.get('model.duration'), 'duration should match');
   assert.equal(this.$('input[type="dollar-amount"]')[0].value, this.get('model.price_small'), 'price for small car should watch');
