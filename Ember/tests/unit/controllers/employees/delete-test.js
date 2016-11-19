3d1586fced1623db19063752f90ce0bc7edb1d0e
Leann Labelle <leann.88.labelle@gmail.com>
2016-11-19 17:06:33 -0500

    Implement tests

--- Ember/tests/unit/controllers/employees/delete-test.js
+++ Ember/tests/unit/controllers/employees/delete-test.js
@@ -1,12 +1,42 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('controller:employees/delete', 'Unit | Controller | employees/delete', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller = this.subject();
+test('#deleteEmployee remains on deletion page following a deletion', function(assert) {
+  let controller = this.subject({
+      model: Ember.Object.create({
+        destroyRecord() {
+          return new Ember.RSVP.Promise(function(resolve) {
+            resolve(true);
+          });
+        }
+      }),
+      transitionToRoute(route) {
+        assert.equal(route, 'employees');
+      }
+  });
+  controller.send('deleteEmployee');
+
   assert.ok(controller);
+});
+
+test('#deleteEmployee remains on employee page following a failed deletion', function(assert) {
+  let controller = this.subject({
+      model: Ember.Object.create({
+        destroyRecord() {
+          return new Ember.RSVP.Promise(function(resolve, reject) {
+            reject({ error: 'could not destroy a record' });
+          });
+        }
+      }),
+      transitionToRoute(route) {
+        assert.equal(route, 'employee');
+      }
+  });
+  assert.throws(controller.send('deleteEmployee'), "throws with just a message, not using the 'expected' argument");
+
 });
--- Ember/tests/unit/controllers/employees/new-test.js
+++ Ember/tests/unit/controllers/employees/new-test.js
@@ -1,12 +1,44 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('controller:employees/new', 'Unit | Controller | employees/new', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller = this.subject();
+test('#saveEmployee transitions to employees', function(assert) {
+  let controller = this.subject({
+      model: Ember.Object.create({
+        save() {
+          return new Ember.RSVP.Promise(function(resolve) {
+            resolve(true);
+          });
+        }
+      }),
+      transitionToRoute(route) {
+        assert.equal(route, 'employees');
+      }
+  });
+
+  controller.send('saveEmployee');
+
   assert.ok(controller);
+});
+
+test('#saveEmployee will NOT transition to employees', function(assert) {
+  let controller = this.subject({
+      model: Ember.Object.create({
+        save() {
+          return new Ember.RSVP.Promise(function(resolve, reject) {
+            reject({ error: 'could not destroy a record' });
+          });
+        }
+      }),
+      transitionToRoute(route) {
+        assert.equal(route, 'employees');
+      }
+  });
+
+  assert.throws(controller.send('saveEmployee'), "throws with just a message, not using the 'expected' argument");
+
 });
