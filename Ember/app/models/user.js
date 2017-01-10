c0a8094c0d6beb2fc472d7005515f9d476dbbacd
Nada Radwan Mohsen <nradwanm@ASHRAFs-MacBook-Air.local>
2017-01-09 21:08:56 -0500

    Ember: added unit tests to user and employee

--- Ember/app/models/user.js
+++ Ember/app/models/user.js
@@ -2,9 +2,9 @@ import DS from 'ember-data';
 
 const { attr, Model, belongsTo } = DS;
 
-export default DS.Model.extend({
-  email: DS.attr('string', { defaultValue: 'not setup' }),
-  password: DS.attr('string', { defaultValue: 'not setup' }),
-  admin: DS.attr('boolean', { defaultValue: false }),
+export default Model.extend({
+  email: attr('string', { defaultValue: 'not setup' }),
+  password: attr('string', { defaultValue: 'not setup' }),
+  admin: attr('boolean', { defaultValue: false }),
   employee:   belongsTo('employee')
 });
