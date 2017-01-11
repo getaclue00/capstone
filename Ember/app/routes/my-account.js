22c0e9bf264a5e041e93bde959a4acdc425bcb5a
Nada Radwan Mohsen <nradwanm@ASHRAFs-MacBook-Air.local>
2017-01-10 23:48:24 -0500

    displaying employee info on my-account page

--- Ember/app/routes/my-account.js
+++ Ember/app/routes/my-account.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+const { service } = Ember.inject;
+
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	currentuser: service('current-user'),
+
+	model(){
+		return this.get('currentuser').user;
+	}
 });
