dc5024635d3ff9589e7b99096346fa703dfc7a4c
Nada Radwan Mohsen <nradwanm@ASHRAFs-MacBook-Air.local>
2017-01-11 18:18:29 -0500

    attempt at fixing the my-account rendering

--- Ember/app/routes/my-account.js
+++ Ember/app/routes/my-account.js
@@ -2,11 +2,21 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 const { service } = Ember.inject;
+const _this = this;
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	currentuser: service('current-user'),
 
 	model(){
-		return this.get('currentuser').user;
+		// let id = _this.get('currentUser');
+
+		//loads model with every template render; therefore, attributes updated
+		return Ember.RSVP.hash({
+	      employees: this.get('store').findAll('employee'),
+	      // user: this.get('store').find('user', id) SUPPOSED TO BE THIS LINE INSTEAD OF THE ONE BELOW
+	      user: this.get('store').find('user', 1)
+    	});
+			
 	}
+
 });
