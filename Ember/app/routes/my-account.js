7173885c7984b365370145463743430e338eecb1
Nada Radwan Mohsen <nradwanm@ASHRAFs-MacBook-Air.local>
2017-01-12 13:24:16 -0500

    fixed populating account info on my-account page

--- Ember/app/routes/my-account.js
+++ Ember/app/routes/my-account.js
@@ -1,24 +1,13 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-const { service } = Ember.inject;
-const _this = this;
-
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-	currentuser: service('current-user'),
 
 	model(){
-		// return this.get('currentuser').user.id;
-
-		// let id = this.get('currentUser');
-
-		//loads model with every template render; therefore, attributes updated
 		return Ember.RSVP.hash({
-	      employees: this.get('store').findAll('employee'),
-	      // user: this.get('store').find('user', id) SUPPOSED TO BE THIS LINE INSTEAD OF THE ONE BELOW
-	      user: this.get('store').find('user', 1)
-    	});
-			
+			user: this.get('store').find('user', this.get('session.data.authenticated.user_id')),
+	        employees: this.get('store').findAll('employee')
+    	});	
 	}
 
 });
