4f63b3a3e3e1c12df6dd4b8e5d4b4d9c7bcea1bc
Alex Kluew <dropbyalex@gmail.com>
2017-01-22 10:29:36 -0500

    Playing with auto-setting end time of appointment

--- Ember/app/components/appointment-editor.js
+++ Ember/app/components/appointment-editor.js
@@ -1,6 +1,21 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
+
+  wasServiceSelected: Ember.computed('model.service', function(){
+    let service = this.get('model.service');
+    let serviceDuration = service.get('duration');
+    let formattedEnd = this.get('model.end');
+
+    if (service.get('name') && service.get('duration')) {
+      let newEndDuration = moment(formattedEnd).add(serviceDuration, 'minutes').format('YYYY-MM-DDTHH:mm');
+      this.get('model').set('formattedEnd', newEndDuration);
+      return true;
+    } else {
+      return false;
+    }
+  }),
 
   aSelectedService: Ember.computed('model.service', function(){
     return this.get('model.service');
