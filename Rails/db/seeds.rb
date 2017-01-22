eb79d5f80efb94097d0ef6bbb1b386dfc28ffbe3
Leann Labelle <leann.88.labelle@gmail.com>
2017-01-22 14:02:05 -0500

    Fix the seeds.rb file

--- Rails/db/seeds.rb
+++ Rails/db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #used in appointments table to reference a deleted employee/car/service and cannot be deleted if referenced by appointments
-Employee.create({ id: '0', last_name: 'DEFAULT', first_name: 'DEFAULT', email: 'DEFAULT', phone_number: '000-000-0000', street_number: '0', street_name: 'DEFUALT', postal_code: 'A0A 0A0'})
-Client.create({ id: '0', last_name: 'DEFAULT', first_name: 'DEFAULT', email: 'DEFAULT', phone_number: '000-000-0000', street_number: '0', street_name: 'DEFUALT', postal_code: 'A0A 0A0'})
+Employee.create({ id: '0', last_name: 'DEFAULT', first_name: 'DEFAULT', phone_number: '000-000-0000', street_number: '0', street_name: 'DEFAULT', postal_code: 'A0A 0A0'})
+Client.create({ id: '0', last_name: 'DEFAULT', first_name: 'DEFAULT', email: 'DEFAULT', phone_number: '000-000-0000', street_number: '0', street_name: 'DEFAULT', postal_code: 'A0A 0A0'})
 Car.create({ id: '0', make: 'DEFAULT', model: 'DEFAULT', size: 'small', interior: 'DEFAULT', colour: 'DEFAULT', client_id: '0'})
 Service.create({id: '0', name: 'DEFAULT', active: false, displayable: false})
 
@@ -17,8 +17,8 @@ Appointment.create({title: 'Someother Appointment', notes: 'Extra notes go here.
 Appointment.create({title: 'Wash Joes Car', notes: 'Car is super new. Needs special buffer compound. Window is tinted.', car_id: '0', service_id: '0'})
 Appointment.create({title: 'Appointment With Employee, Car, and Service', notes: 'Car is super new. Needs special buffer compound. Window is tinted.', car_id: 0, service_id: 0, employee_id: 0})
 
-Employee.create({ id: '1', last_name: 'Testing', first_name: 'Tester', email: 'test@test.com', phone_number: '000-000-0000', street_number: '0', street_name: 'DEFUALT', postal_code: 'A0A 0A0'})
-Employee.create({ id: '2', last_name: 'Wayne', first_name: 'Bruce', email: 'bat@batman.com', phone_number: '000-000-0000', street_number: '0', street_name: 'DEFUALT', postal_code: 'A0A 0A0'})
+Employee.create({ id: '1', last_name: 'Testing', first_name: 'Tester', phone_number: '000-000-0000', street_number: '0', street_name: 'DEFAULT', postal_code: 'A0A 0A0'})
+Employee.create({ id: '2', last_name: 'Wayne', first_name: 'Bruce', phone_number: '000-000-0000', street_number: '0', street_name: 'DEFAULT', postal_code: 'A0A 0A0'})
 User.create({email: 'test@test.com', password: 'password', admin: true, employee_id: '1'})
 User.create({email: 'batman@batman.com', password: 'password', admin: false, employee_id: '2'})
 
