5bfdcd84a442ebcf50afe6b2d7367535189a22ea
Nada Radwan Mohsen <radwanmohsenn@gmail.com>
2017-01-25 13:44:28 -0500

    fixing component test

--- Ember/tests/integration/components/user-account-editor-test.js
+++ Ember/tests/integration/components/user-account-editor-test.js
@@ -1,25 +1,53 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
-moduleForComponent('user-account-editor', 'Integration | Component | user account editor', {
+const UserStub = Ember.Object.extend({
+  getEmail() {
+    return this.get('email');
+  },
+
+  getPassword() {
+    return this.get('password');
+  },
+
+  getAdmin() {
+    return this.get('admin');
+  }
+
+});
+
+let user = UserStub.create({
+  email: 'test@test.com',
+  password: 'blabla',
+  admin: true,
+  confirm: 'blabla'
+});
+
+moduleForComponent('user-account-editor-editor', 'Integration | Component | user account editor', {
   integration: true
 });
 
-test('it renders', function(assert) {
-
+test('it renders a view with a model', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('model', user);
 
-  this.render(hbs`{{user-account-editor}}`);
+  assert.expect(7);
 
-  assert.equal(this.$().text().trim(), '');
+  this.render(hbs`{{
+    user-account-editor
+    model=model
+  }}`);
 
-  // Template block usage:
-  this.render(hbs`
-    {{#user-account-editor}}
-      template block text
-    {{/user-account-editor}}
-  `);
+  // Test in correct inputs bar are present
+  assert.equal(this.$('input[type="password"]').length, 2, 'should be only 2 input password field - new and confirm password');
+  assert.equal(this.$('select').length, 1, 'should be only 1 yes or no input for admin attribute of user');
+  assert.equal(this.$('input[type="name"]').length, 1, 'should be only 1 input name field - email');
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  // Test if proper values are placed into form
+  assert.equal(this.$('input[type="name"]').val(), this.get('model.email'), 'names should match');
+  assert.equal(this.$('select')[0].value, this.get('model.admin').toString(), 'Whether a user is admin should match');
+  assert.equal(this.$('input[type="password"]')[0].value, this.get('model.password').toString(), 'password should match');
+  assert.equal(this.$('input[type="password"]')[1].value, this.get('model.confirm').toString(), 'password should match');
 });
