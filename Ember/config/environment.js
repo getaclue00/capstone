fc9edada6acf84de19941879f1bca9f161e7901e
Alex Kluew <dropbyalex@gmail.com>
2017-01-28 11:22:36 -0500

    began update to ember 211

--- Ember/config/environment.js
+++ Ember/config/environment.js
@@ -10,6 +10,10 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
 
@@ -30,8 +34,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV['ember-cli-mirage'] = {
-      enabled: true
-    }
+      enabled: false
+    };
   }
 
   if (environment === 'test') {
