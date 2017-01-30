cdbe66822a658942d65fb9d4e69e553fbec1fcef
Leann Labelle <leann.88.labelle@gmail.com>
2017-01-30 10:48:58 -0500

    removed addClass function

--- Ember/app/routes/my-account.js
+++ Ember/app/routes/my-account.js
@@ -8,19 +8,3 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	}
 
 });
-
-Ember.Route.reopen({
-  activate: function() {
-    var cssClass = this.toCssClass();
-    console.log(cssClass);
-    if (cssClass !== 'application') {
-      Ember.$('body').addClass(cssClass);
-    }
-  },
-  deactivate: function() {
-    Ember.$('body').removeClass(this.toCssClass());
-  },
-  toCssClass: function() {
-    return this.routeName.replace(/\./g, '-').dasherize();
-  }
-});
