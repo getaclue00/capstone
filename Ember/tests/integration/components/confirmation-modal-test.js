55e9f2ecc6ca92efba73abf345b8850d0ba37fb6
Leann Labelle <leann.88.labelle@gmail.com>
2017-02-01 09:54:31 -0500

    Fix confirmation modal test

--- Ember/tests/integration/components/confirmation-modal-test.js
+++ Ember/tests/integration/components/confirmation-modal-test.js
@@ -1,5 +1,46 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+const ServiceStub = Ember.Object.extend({
+  getName() {
+    return this.get('name');
+  },
+
+  getPriceSmall() {
+    return this.get('price_small');
+  },
+
+  getPriceLarge() {
+    return this.get('price_large');
+  },
+
+  getDuration() {
+    return this.get('duration');
+  },
+
+  getDescription() {
+    return this.get('description');
+  },
+  getActive() {
+    return this.get('active');
+  },
+  getDisplayable() {
+    return this.get('displayable');
+  },
+
+});
+
+let service = ServiceStub.create({
+  name: 'Clean Car',
+  price_small: '100',
+  price_large: '120',
+  duration: '60',
+  description: 'Cleaning the car',
+  active: true,
+  displayable: false,
+});
+
 
 moduleForComponent('confirmation-modal', 'Integration | Component | confirmation modal', {
   integration: true
@@ -9,8 +50,10 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{confirmation-modal type='service'}}`);
+  this.set('model', service);
 
-  assert.equal(this.$().text().trim(), 'Delete the following service?');
+  this.render(hbs`{{confirmation-modal model=model type='service'}}`);
+
+  assert.equal(this.$().text().trim(), 'Delete the following service?' + '\n  Clean Car');
 
 });
