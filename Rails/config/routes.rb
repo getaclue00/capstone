2904b5c4d7bbcf2cab8d7b4bb72cd909480b51f1
Alex Kluew <dropbyalex@gmail.com>
2017-02-05 02:04:16 -0500

    playing with routes

--- Rails/config/routes.rb
+++ Rails/config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
   scope '/api' do
-    post "users", to: "users#create", constraints: lambda { |req| req.format == :json }
-    patch "/users/:id" => "users#update", constraints: lambda { |req| req.format == :json }
+    # constraints: lambda { |req| req.format == :json }
+    post "users", to: "users#create"
+    patch "/users/:id" => "users#update"
 
     devise_for :users, controllers: {
       sessions: 'sessions'
     }
-    get "session/csrf", to: "session#csrf", constraints: lambda { |req| req.format == :json }
+    get "session/csrf", to: "session#csrf"
 
     #creating the RESTful resources
-    resources :clients, constraints: lambda { |req| req.format == :json }
-    resources :employees, constraints: lambda { |req| req.format == :json }
-    resources :cars, constraints: lambda { |req| req.format == :json }
-    resources :services, constraints: lambda { |req| req.format == :json }
-    resources :appointments, constraints: lambda { |req| req.format == :json }
-    get "users", to: 'users#index', constraints: lambda { |req| req.format == :json }
-    get "users/:id", to: 'users#show', constraints: lambda { |req| req.format == :json }
-    delete "/users/:id" => "users#destroy", constraints: lambda { |req| req.format == :json }
+    resources :clients
+    resources :employees
+    resources :cars
+    resources :services
+    resources :appointments
+    get "users", to: 'users#index'
+    get "users/:id", to: 'users#show'
+    delete "/users/:id" => "users#destroy"
   end
 
   # putting wildcard back
