3798e1b9d7f658a77c296fb92377ae27989a2486
Leann Labelle <leann.88.labelle@gmail.com>
2017-02-06 15:59:52 -0500

    Edit form for mobile device

--- Ember/app/components/booking-appointment-wizard.js
+++ Ember/app/components/booking-appointment-wizard.js
@@ -2,6 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement() {
-    this.$('#smartwizard').smartWizard();
-  }
+    this.$('#smartwizard').smartWizard({
+      reverseButtonsOrder: true,
+      // toolbarSettings: {
+      //   toolbarExtraButtons: [
+      //     $('<button></button>').text('Finish')
+      //     .addClass('btn btn-info')
+      //     .on('click', function(){
+      //     alert('Finish button click');
+      //     }),
+      //   ]
+      // }
+    });
+  },
+
 });
