be9070a7383a6cc93adbc9787a08559c5f3dca7c
Nada Radwan Mohsen <radwanmohsenn@gmail.com>
2017-02-06 22:39:33 -0500

    testing employee controller property

--- Ember/tests/unit/controllers/employees/delete-test.js
+++ Ember/tests/unit/controllers/employees/delete-test.js
@@ -1,11 +1,42 @@
-// import { moduleFor, test } from 'ember-qunit';
+import { moduleFor, test } from 'ember-qunit';
 // import Ember from 'ember';
-//
-// moduleFor('controller:employees/delete', 'Unit | Controller | employees/delete', {
-//   // Specify the other units that are required for this test.
-//   // needs: ['controller:foo']
-// });
-//
+
+moduleFor('controller:employees/delete', 'Unit | Controller | employees/delete', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('checking type', function(assert) {
+  assert.expect(1);
+
+  // get the controller instance
+  const ctrl = this.subject();
+
+  // check the type
+  assert.equal(ctrl.get('type'), 'employee', 'type properly set');
+});
+
+
+// postCtrl.set('model', Ember.Object.create({ title: 'foo' }));
+
+test('#deleteEmployee deletes the employee', function(assert) {
+  const ctrl = this.subject({
+      model: Ember.Object.create({
+        destroyRecord() {
+          return new Ember.RSVP.Promise(function(resolve) {
+            resolve(true);
+          });
+        }
+      }),
+      transitionToRoute(route) {
+        assert.equal(route, 'employees');
+      }
+  });
+  ctrl.send('deleteEmployee');
+
+  assert.ok(ctrl);
+});
+
 // test('#deleteEmployee remains on deletion page following a deletion', function(assert) {
 //   let controller = this.subject({
 //       model: Ember.Object.create({
