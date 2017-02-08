2c32e728f04b7ae2266b3685d53690f2fe9cb7e4
Leann Labelle <leann.88.labelle@gmail.com>
2017-02-08 17:40:48 -0500

    Additional work on time selection of appointment

--- Ember/app/components/appointment-time-selection.js
+++ Ember/app/components/appointment-time-selection.js
@@ -1,6 +1,22 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
+
+  // startTime: moment('8:00 AM').format('HH:mm A'),
+  // endTime: moment('5:00 PM').format('HH:mm A'),
+  availableTimes: Ember.computed('params.[]', function(){
+    var arrayTime = new Array();
+
+    for(var i = 0; i < 5; i++){
+       console.log("s");
+      arrayTime.push(moment("123", "hmm").format("HH:mm"));
+    }
+
+    return arrayTime;
+
+  }),
+
  didInsertElement() {
     this._super(...arguments);
     Ember.$('#appointment-time-selection').modal('show');
