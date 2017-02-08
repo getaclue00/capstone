072fefc9d9ff29f1e5c29e82ce853bd3030615d4
Nada Radwan Mohsen <radwanmohsenn@gmail.com>
2017-02-07 23:10:18 -0500

    attempt at resolving assertion problem

--- Ember/tests/unit/controllers/employees/delete-test.js
+++ Ember/tests/unit/controllers/employees/delete-test.js
@@ -1,5 +1,5 @@
 import { moduleFor, test } from 'ember-qunit';
-// import Ember from 'ember';
+import Ember from 'ember';
 
 moduleFor('controller:employees/delete', 'Unit | Controller | employees/delete', {
   // Specify the other units that are required for this test.
@@ -8,8 +8,6 @@ moduleFor('controller:employees/delete', 'Unit | Controller | employees/delete',
 
 test('checking type', function(assert) {
   assert.expect(1);
-
-  // get the controller instance
   const ctrl = this.subject();
 
   // check the type
@@ -17,7 +15,6 @@ test('checking type', function(assert) {
 });
 
 
-// postCtrl.set('model', Ember.Object.create({ title: 'foo' }));
 
 test('#deleteEmployee deletes the employee', function(assert) {
   const ctrl = this.subject({
@@ -29,45 +26,32 @@ test('#deleteEmployee deletes the employee', function(assert) {
         }
       }),
       transitionToRoute(route) {
-        assert.equal(route, 'employees');
+     
+      	assert.equal(route, 'employees');
       }
   });
+
   ctrl.send('deleteEmployee');
 
-  assert.ok(ctrl);
+  assert.ok(ctrl); //passes if first argument is T
 });
 
-// test('#deleteEmployee remains on deletion page following a deletion', function(assert) {
-//   let controller = this.subject({
-//       model: Ember.Object.create({
-//         destroyRecord() {
-//           return new Ember.RSVP.Promise(function(resolve) {
-//             resolve(true);
-//           });
-//         }
-//       }),
-//       transitionToRoute(route) {
-//         assert.equal(route, 'employees');
-//       }
-//   });
-//   controller.send('deleteEmployee');
-//
-//   assert.ok(controller);
-// });
-//
-// test('#deleteEmployee remains on employee page following a failed deletion', function(assert) {
-//   let controller = this.subject({
-//       model: Ember.Object.create({
-//         destroyRecord() {
-//           return new Ember.RSVP.Promise(function(resolve, reject) {
-//             reject({ error: 'could not destroy a record' });
-//           });
-//         }
-//       }),
-//       transitionToRoute(route) {
-//         assert.equal(route, 'employee');
-//       }
-//   });
-//   assert.throws(controller.send('deleteEmployee'), "throws with just a message, not using the 'expected' argument");
-//
-// });
+
+
+test('#deleteEmployee remains on employee page following a failed deletion', function(assert) {
+  let ctrl = this.subject({
+      model: Ember.Object.create({
+        destroyRecord() {
+          return new Ember.RSVP.Promise(function(resolve, reject) {
+            reject({ error: 'could not destroy a record' });
+          });
+        }
+      })
+  });
+  assert.throws(ctrl.send('deleteEmployee'),  		
+	{
+	  "message": "Cannot read property 'call' of undefined",
+	  "name": "TypeError"
+	},
+    "throws with just a message, not using the 'expected' argument");
+});
