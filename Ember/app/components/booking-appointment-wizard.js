7596d2ac867a6374f51b87e2b67a7daa5eb0d069
Leann Labelle <leann.88.labelle@gmail.com>
2017-02-08 12:14:27 -0500

    Add calendar to date selection

--- Ember/app/components/booking-appointment-wizard.js
+++ Ember/app/components/booking-appointment-wizard.js
@@ -4,15 +4,28 @@ export default Ember.Component.extend({
   didInsertElement() {
     this.$('#smartwizard').smartWizard({
       reverseButtonsOrder: true,
-      // toolbarSettings: {
-      //   toolbarExtraButtons: [
-      //     $('<button></button>').text('Finish')
-      //     .addClass('btn btn-info')
-      //     .on('click', function(){
-      //     alert('Finish button click');
-      //     }),
-      //   ]
-      // }
-    }),
+    });
   },
+
+  viewName: 'month',
+
+  businessHours: {
+    // days of week. an array of zero-based day of week integers (0=Sunday)
+    dow: [ 1, 2, 3, 4, 5 ], // Monday - Thursday
+
+    start: '10:00', // a start time (10am in this example)
+    end: '18:00', // an end time (6pm in this example)
+  },
+
+ header: {
+    left:   'today',
+    center: 'title',
+    right:  'prev,next'
+  },
+
+  actions: {
+    handleCalendarDayClick() {
+      console.log("hi");
+    }
+  }
 });
