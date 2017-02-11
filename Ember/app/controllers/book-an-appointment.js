5b35209faa0794f9e7a048bb685504393688ae89
Leann Labelle <leann.88.labelle@gmail.com>
2017-02-11 10:42:51 -0500

    allow selection of service

--- Ember/app/controllers/book-an-appointment.js
+++ Ember/app/controllers/book-an-appointment.js
@@ -20,11 +20,18 @@ export default Ember.Controller.extend({
   },
 
   actions: {
-    handleCalendarDayClick(date) {
+    selectService(service) {
+      if(!Ember.isEmpty(service)){
+        this.get('appointment').set('service', service);
+      }
+    },
+
+    handleCalendarDayClick(date, jsEvent) {
       if (moment().format('YYYY-MM-DD') === date.format('YYYY-MM-DD') || date.isAfter(moment())) {
         if(this.get('businessHours.dow').indexOf(parseInt(date.format('e'), 10)) > -1){
           this.set('selectedDate', moment(date, 'YYYY-MM-DD').format('MMMM D, YYYY'));
           this.set('selectTime', true);
+          $(jsEvent.target).closest('.fc-row.fc-week.fc-widget-content.fc-bg.td.fc-day').find('.fc-day').addClass('selectedDay');
         }
       }
     }
