bf3e355d66c3f309b0c7fbf69f2e4110ba94fdd9
Leann Labelle <leann.88.labelle@gmail.com>
2017-02-19 08:16:27 -0500

    Fixing toggling of popovers

--- Ember/app/routes/book-an-appointment.js
+++ Ember/app/routes/book-an-appointment.js
@@ -34,7 +34,6 @@ export default Ember.Route.extend(UnauthenticatedRouteMixin, {
     controller.setProperties(models);
   },
 
-
   renderSmartWizard() {
     Ember.$('#smartwizard').smartWizard({
       reverseButtonsOrder: true,
@@ -42,8 +41,21 @@ export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   },
 
   enablePopovers () {
+
+    // Initialized popovers
     Ember.$("a[rel=popover]").popover().click(function(e) {
       e.preventDefault();
+    });
+
+    // Ensure they close when page is clicked
+    Ember.$(document).on('click', function (e) {
+      Ember.$('[data-toggle="popover"]').each(function () {
+
+        if (!Ember.$(this).is(e.target) && Ember.$(this).has(e.target).length === 0 && Ember.$('.popover').has(e.target).length === 0) {
+            ((Ember.$(this).popover('hide').data('bs.popover')||{}).inState||{}).click = false;
+        }
+
+      });
     });
   },
 
