fd0911ad22603228d4c2c28f613b1933a8cca714
Leann Labelle <leann.88.labelle@gmail.com>
2017-02-21 02:30:00 -0500

    small bracket

--- Ember/app/components/management-tables.js
+++ Ember/app/components/management-tables.js
@@ -13,6 +13,7 @@ export default Ember.Component.extend({
 
   actions: {
     viewOperations(id){
-      Ember.$(`tr.${id} td div.management-operation`).toggle( "slow");    }
+      Ember.$(`tr.${id} td div.management-operation`).toggle( "slow");
+    }
   }
 });
