a9b0f045ef7a0e6d192f97d4834dc80e091033cc
Leann Labelle <leann.88.labelle@gmail.com>
2017-03-05 19:32:56 -0500

    Finish client form for mobile user

--- Ember/app/components/client-information-form.js
+++ Ember/app/components/client-information-form.js
@@ -1,21 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  flashMessages: Ember.inject.service(),
   didInsertElement() {
     this._super(...arguments);
   },
 
   actions: {
     verifyInformation(){
+      var flashMessages = this.get('flashMessages');
       var isValid = true;
       Ember.$('input').each(function() {
-        if ( $(this).val() === '' )
+        if (Ember.$(this).val() === ''){
             isValid = false;
-      });
+            flashMessages.danger("Please Enter Your " + Ember.$(this)[0].name);
+          }
+        });
 
       if(isValid){
          Ember.$('#move-to-confirmation').removeAttr('disabled');
+         flashMessages.success("Information entered correctly");
       }
     }
   }
