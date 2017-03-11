dcf2b96799b37cc4dd0dee8c5731b3fda26b61b5
Alex Kluew <dropbyalex@gmail.com>
2017-03-11 17:08:56 -0500

    some progress

--- Ember/app/routes/my-work-availability.js
+++ Ember/app/routes/my-work-availability.js
@@ -1,32 +1,46 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-let data = Ember.Object.create({
-  showMonday: false,
-  mondayOpen: '08:00:00',
-  mondayClose: '17:00:00',
-  showTuesday: true,
-  tuesdayOpen: '08:00:00',
-  tuesdayClose: '17:00:00',
-  showWednesday: false,
-  wednesdayOpen: '08:00:00',
-  wednesdayClose: '17:00:00',
-  showThursday: false,
-  thursdayOpen: '08:00:00',
-  thursdayClose: '17:00:00',
-  showFriday: false,
-  fridayOpen: '08:00:00',
-  fridayClose: '17:00:00',
-  showSaturday: true,
-  saturdayOpen: '08:00:00',
-  saturdayClose: '17:00:00',
-  showSunday: false,
-  sundayOpen: '08:00:00',
-  sundayClose: '17:00:00',
-});
-
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model() {
-    return data;
+    return this.get('store').createRecord('companyPreferences');
+  },
+
+  actions: {
+    toggleMonday(preferences) {
+      if (!Ember.isEmpty(preferences)) {
+        preferences.toggleProperty('workMonday');
+      }
+    },
+    toggleTuesday(preferences) {
+      if (!Ember.isEmpty(preferences)) {
+        preferences.toggleProperty('workTuesday');
+      }
+    },
+    toggleWednesday(preferences) {
+      if (!Ember.isEmpty(preferences)) {
+        preferences.toggleProperty('workWednesday');
+      }
+    },
+    toggleThursday(preferences) {
+      if (!Ember.isEmpty(preferences)) {
+        preferences.toggleProperty('workThursday');
+      }
+    },
+    toggleFriday(preferences) {
+      if (!Ember.isEmpty(preferences)) {
+        preferences.toggleProperty('workFriday');
+      }
+    },
+    toggleSaturday(preferences) {
+      if (!Ember.isEmpty(preferences)) {
+        preferences.toggleProperty('workSaturday');
+      }
+    },
+    toggleSunday(preferences) {
+      if (!Ember.isEmpty(preferences)) {
+        preferences.toggleProperty('workSunday');
+      }
+    }
   }
 });
