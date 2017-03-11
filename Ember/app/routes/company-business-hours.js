e80cc5c07fc095a620b87ac9c6f42665195105bf
Alex Kluew <dropbyalex@gmail.com>
2017-03-11 03:00:05 -0500

    some more ideas to show the toggling mechanism

--- Ember/app/routes/company-business-hours.js
+++ Ember/app/routes/company-business-hours.js
@@ -1,5 +1,32 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+let data = Ember.Object.create({
+  showMonday: true,
+  mondayOpen: '08:00:00',
+  mondayClose: '17:00:00',
+  showTuesday: false,
+  tuesdayOpen: '08:00:00',
+  tuesdayClose: '17:00:00',
+  showWednesday: false,
+  wednesdayOpen: '08:00:00',
+  wednesdayClose: '17:00:00',
+  showThursday: false,
+  thursdayOpen: '08:00:00',
+  thursdayClose: '17:00:00',
+  showFriday: false,
+  fridayOpen: '08:00:00',
+  fridayClose: '17:00:00',
+  showSaturday: false,
+  saturdayOpen: '08:00:00',
+  saturdayClose: '17:00:00',
+  showSunday: true,
+  sundayOpen: '08:00:00',
+  sundayClose: '17:00:00',
+});
+
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model() {
+    return data;
+  }
 });
--- Ember/app/routes/my-work-availability.js
+++ Ember/app/routes/my-work-availability.js
@@ -1,5 +1,32 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+let data = Ember.Object.create({
+  showMonday: false,
+  mondayOpen: '08:00:00',
+  mondayClose: '17:00:00',
+  showTuesday: true,
+  tuesdayOpen: '08:00:00',
+  tuesdayClose: '17:00:00',
+  showWednesday: false,
+  wednesdayOpen: '08:00:00',
+  wednesdayClose: '17:00:00',
+  showThursday: false,
+  thursdayOpen: '08:00:00',
+  thursdayClose: '17:00:00',
+  showFriday: false,
+  fridayOpen: '08:00:00',
+  fridayClose: '17:00:00',
+  showSaturday: true,
+  saturdayOpen: '08:00:00',
+  saturdayClose: '17:00:00',
+  showSunday: false,
+  sundayOpen: '08:00:00',
+  sundayClose: '17:00:00',
+});
+
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model() {
+    return data;
+  }
 });
