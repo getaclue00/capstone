c10802f11db5d2fa23cc05f276494656e045efa6
Alex Kluew <dropbyalex@gmail.com>
2017-03-14 17:39:10 -0400

    fixing ember tests

--- Ember/tests/unit/models/employee-test.js
+++ Ember/tests/unit/models/employee-test.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 moduleForModel('employee', 'Unit | Model | employee', {
   // Specify the other units that are required for this test.
-  needs: ['model:appointment', 'model:user']
+  needs: ['model:appointment', 'model:user', 'model:company-preference']
 });
 
 test('checking fullName', function(assert) {
@@ -32,10 +32,13 @@ test('should own appointments', function(assert) {
   assert.deepEqual(relationship.kind, 'hasMany', 'kind of relationship is hasMany');
 });
 
-test('should belong to a user', function(assert){
+test('should belong to a user and company-preference', function(assert){
 	const Employee = this.store().modelFor('employee');
-	const relationship = Ember.get(Employee, 'relationshipsByName').get('user');
+	const relationshipUser = Ember.get(Employee, 'relationshipsByName').get('user');
+	const relationshipCompanyPreference = Ember.get(Employee, 'relationshipsByName').get('companyPreference');
 
-	assert.deepEqual(relationship.key, 'user', 'has relationship with user');
-	assert.deepEqual(relationship.kind, 'belongsTo', 'kind of relationship is belongsTo');
+	assert.deepEqual(relationshipUser.key, 'user', 'has relationship with user');
+	assert.deepEqual(relationshipUser.kind, 'belongsTo', 'kind of relationship is belongsTo');
+  assert.deepEqual(relationshipCompanyPreference.key, 'companyPreference', 'has relationship with company-preference');
+	assert.deepEqual(relationshipCompanyPreference.kind, 'belongsTo', 'kind of relationship is belongsTo');
 });
