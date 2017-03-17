d2dea63d15e37f34c08cd4f5f2253265863489ab
Nada Radwan Mohsen <radwanmohsenn@gmail.com>
2017-03-16 23:02:32 -0400

    booking controller tests

--- Ember/tests/unit/controllers/booking-history-test.js
+++ Ember/tests/unit/controllers/booking-history-test.js
@@ -5,8 +5,27 @@ moduleFor('controller:booking-history', 'Unit | Controller | booking history', {
   // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
+test('checking arrays and hash maps', function(assert) {
+  assert.expect(4);
+  const ctrl = this.subject();
+
+  assert.deepEqual(ctrl.get('headers'),
+    ["Service", "Employee", "Client",
+            "Start Time", "End Time", "Cost", "Location", "Status", "Notes"],
+    'header properly set');
+
+  assert.deepEqual(ctrl.get('attributes'),
+    {"service.name": "Service", "employee.fullName" : "Employee", "client.fullName" : "Client",
+                "formattedStart": "Start Time", "formattedEnd": "End Time", "cost": "Cost", "location": "Location",
+                "status": "Status", "notes": "Notes"},
+    'attributes properly set');
+
+  assert.deepEqual(ctrl.get('operations'),
+    {'appointment-history': 'glyphicon-book'},
+   'operations properly set');
+
+  assert.equal(ctrl.get('collection'),
+    'appointments',
+   'operations properly set');
 });
+
