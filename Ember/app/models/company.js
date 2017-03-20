68dfb56b458856add4b84f8cf29f639c5b20b63d
Alex Kluew <dropbyalex@gmail.com>
2017-03-20 12:40:37 -0400

    removing inline styling and added attribute to company

--- Ember/app/models/company.js
+++ Ember/app/models/company.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 const { Model, attr, hasMany } = DS;
+const { computed } = Ember;
 
 export default Model.extend({
   workMonday:     attr('boolean', { defaultValue: false }),
@@ -26,5 +28,16 @@ export default Model.extend({
   sundayClose:    attr('string', { defaultValue: '17:00:00' }),
   employee:       hasMany('employee'),
   contactEmail:   attr('string', { defaultValue: 'segradetailingcapstoneteam@gmail.com' }),
-  contactPhoneNumber: attr('string')
+  contactPhoneNumber: attr('string'),
+  daysOfWeekNotWorking:   computed('workMonday', 'workTuesday', 'workWednesday', 'workThursday', 'workFriday', 'workSaturday', 'workSunday', function() {
+    let result = [];
+    if (!this.get('workSunday'))    { result.push('0'); }
+    if (!this.get('workMonday'))    { result.push('1'); }
+    if (!this.get('workTuesday'))   { result.push('2'); }
+    if (!this.get('workWednesday')) { result.push('3'); }
+    if (!this.get('workThursday'))  { result.push('4'); }
+    if (!this.get('workFriday'))    { result.push('5'); }
+    if (!this.get('workSaturday'))  { result.push('6'); }
+    return result.toString();
+  })
 });
