42d9eb66341cb47d060957a31a017a4ee2f706dc
Leann Labelle <leann.88.labelle@gmail.com>
2017-03-28 20:11:33 -0400

    Changing algorithm to take into account time to complete service

--- Ember/app/components/appointment-time-selection.js
+++ Ember/app/components/appointment-time-selection.js
@@ -48,8 +48,8 @@ export default Ember.Component.extend({
           var employeeStart = result.get('firstObject').get(dayOfTheWeek.toLowerCase() + "Open");
           var employeeEnd = result.get('firstObject').get(dayOfTheWeek.toLowerCase() + "Close");
           var employeeWorking = result.get('firstObject').get("work" + dayOfTheWeek);
-          var timeDiff = moment(employeeEnd,"h:mma").diff(moment(employeeStart,"h:mma"));
-          var time = employeeStart;
+          var timeDiff = moment(employeeEnd, 'h:mma').diff(moment(employeeStart, 'h:mma'));
+          var time = moment(employeeStart, 'h:mm').format('h:mma');
           self.get('store').query('appointment', {
             filter: {
               date:        moment(self.get('selectedDate'),'MMMM D, YYYY').format('YYYY-MM-DD'),
@@ -57,23 +57,29 @@ export default Ember.Component.extend({
             }
           }).then(function(results) {
             if(employeeWorking) {
-              var apptCounter=0;
+              var apptCounter = 0;
               for(var i = 0 ; i < timeDiff; i+=1800000){
                 if(results.objectAt(apptCounter)){
-                  var apptStart = moment(results.objectAt(apptCounter).get('start')).format("h:mma");
+                  var service = results.objectAt(apptCounter).get('service');
+                  var apptStart = moment(results.objectAt(apptCounter).get('start')).format('h:mma');
+                  var apptCheck = moment(time, 'h:mma').add(service.get('duration') + service.get('bufferTime'), 'minutes')
+                   .format('h:mma');
+                  // An appointment already exist is this time slot
                   if(apptStart === time) {
-                    var apptEnd = moment(results.objectAt(apptCounter).get('end')).format("h:mma");
-                    var apptDiff = moment(apptEnd,"h:mma").diff(moment(apptStart,"h:mma"));
-                    time = moment(apptEnd,"h:mma");
+                    var apptEnd = moment(results.objectAt(apptCounter).get('end')).format('h:mma');
+                    var apptDiff = moment(apptEnd, 'h:mma').diff(moment(apptStart, 'h:mma'));
+                    time = moment(apptEnd,'h:mma').format('h:mma');
                     i+= apptDiff;
                     apptCounter++;
+                  } else if (apptCheck > apptStart){ // Not enough time to complete appointment
+                    time = moment(time, 'h:mma').add(30, 'minutes').format('h:mma');
                   } else {
-                    arrayTime.push(moment(time, "h:mma").format("h:mma"));
-                    time = moment(time, "h:mma").add(30, 'minutes').format("h:mma");
+                    arrayTime.push(moment(time, 'h:mma').format('h:mma'));
+                    time = moment(time, 'h:mma').add(30, 'minutes').format('h:mma');
                   }
                 } else {
-                  arrayTime.push(moment(time, "h:mma").format("h:mma"));
-                  time = moment(time, "h:mma").add(30, 'minutes').format("h:mma");
+                  arrayTime.push(moment(time, "h:mma").format('h:mma'));
+                  time = moment(time, 'h:mma').add(30, 'minutes').format('h:mma');
                 }
               }
             }
