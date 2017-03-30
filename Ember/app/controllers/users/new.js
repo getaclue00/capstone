6eb7d753568e670ff02bef23525e8bce01b56278
Nada Radwan Mohsen <radwanmohsenn@gmail.com>
2017-03-30 11:20:55 -0400

    setting password and confirmation to undefined on closinh component

--- Ember/app/controllers/users/new.js
+++ Ember/app/controllers/users/new.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   flashMessages: Ember.inject.service(),
+  confirm: undefined,
 
 	actions: {
     // Action for saving a new service
@@ -25,12 +26,15 @@ export default Ember.Controller.extend({
       }
 
 
-      if (self.get('model').get('confirm') === self.get('model').get('password')){
-          user.save().then(onSuccessful).catch(onError);
-	    }else{
-	    	window.scrollTo(0,0);
+      console.log("from controller**********************");
+      console.log("confrirm is "+self.get('confirm'));
+      console.log("password is "+self.get('model').get('password') );
+      if (self.get('model').get('password') === self.get('confirm')){
+        self.get('model').save().then(onSuccessful).catch(onError);
+      }else{
+        window.scrollTo(0,0);
         flashMessages.danger('Error: passwords do not match!');
-	    }
+      }
     }
   }
 });
--- Ember/app/controllers/users/show.js
+++ Ember/app/controllers/users/show.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   flashMessages: Ember.inject.service(),
+  confirm: undefined,
 	actions: {
 
     updateUser() {
@@ -21,12 +22,22 @@ export default Ember.Controller.extend({
         flashMessages.danger('Error: '+ message);
       }
 
-      if (this.get('model').get('confirm') === this.get('model').get('password')){
-	    	this.get('model').save().then(onSuccessful).catch(onError);
-	    }else{
-	    	window.scrollTo(0,0);
+     //  if (self.get('model').get('confirm') === self.get('model').get('password')){
+	    // 	self.get('model').save().then(onSuccessful).catch(onError);
+	    // }else{
+	    // 	window.scrollTo(0,0);
+     //    flashMessages.danger('Error: passwords do not match!');
+	    // }
+      console.log("from controller**********************");
+      console.log("confrirm is "+self.get('confirm'));
+      console.log("password is "+self.get('model').get('password') );
+      if (self.get('model').get('password') === self.get('confirm')){
+        self.get('model').save().then(onSuccessful).catch(onError);
+      }else{
+        //dont display error if password is
+        window.scrollTo(0,0);
         flashMessages.danger('Error: passwords do not match!');
-	    }
+      }
     }
   }
 });
