744119a707c9ddfe7e8a262812f63de3041d2d4f
Alex Kluew <dropbyalex@gmail.com>
2017-03-31 01:08:09 -0400

    added currentUser to employees/index

--- Ember/app/controllers/employees/index.js
+++ Ember/app/controllers/employees/index.js
@@ -1,12 +1,38 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  headers: ["Name", "Employee ID", "Phone",
-            "Start Date", "End Date"],
-  attributes: {"fullName": "Name", "id" : "Employee ID:", "phoneNumber" : "Phone",
-                "startDate": "Start Date", "endDate": "End Date"},
+const { inject: { service }, Controller, computed } = Ember;
 
-  operations: {'employees.show': 'glyphicon-pencil', 'employees.delete': 'glyphicon-remove'},
+export default Controller.extend({
+  session:      service(),
+  currentUser:  service('current-user'),
+
+  headers: [
+    "Name",
+    "Employee ID",
+    "Phone",
+    "Start Date",
+    "End Date"
+  ],
+
+  attributes: {
+    "fullName": "Name",
+    "id" : "Employee ID:",
+    "phoneNumber" : "Phone",
+    "startDate": "Start Date",
+    "endDate": "End Date"
+  },
+
+  operations: computed('currentUser.user.admin', function() {
+    let isAdmin = this.get('currentUser.user.admin');
+    if (isAdmin) {
+      return {
+        'employees.show':'glyphicon-pencil',
+        'employees.delete':'glyphicon-remove'
+      };
+    } else {
+      return { };
+    }
+  }),
 
   collection: 'employees'
 });
